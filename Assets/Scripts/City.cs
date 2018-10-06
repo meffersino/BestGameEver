67b81c3edb7983d2121f1808a6ff35f7f0511855
meffersino <michaelpjeffrey@hotmail.co.uk>
2018-10-06 20:35:41 +0100

    more code

---
+++ Assets/Scripts/City.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class City : MonoBehaviour {
+    public int Cash { get; set; }
+    public int Day { get; set; }
+    public int PopulationCurrent { get; set; }
+    public int PopulationCeiling { get; set; }
+    public int JobsCurrent { get; set; }
+    public int JobsCeiling { get; set; }
+
+    public int[] buildingCounts = new int[4];
+    private UIController uiController;
+
+
+
+
+    // Use this for initialization
+    void Start () {
+        uiController = GetComponent<UIController>();
+        Cash = 100;
+        JobsCeiling = 10;
+	}
+	public void EndTurn()
+    {
+        Day++;
+        CalculateCash();
+        CalculatePopulation();
+        CalculateJobs();
+        Debug.Log("Day Ended");
+        uiController.UpdateCityData();
+        uiController.UpdateDayCount();
+        Debug.LogFormat("Jobs: {0}/{1}, Cash: {2}, pop: {3}/{4}",
+            JobsCurrent, JobsCeiling, Cash, PopulationCurrent, PopulationCeiling);
+    }
+
+    void CalculateJobs()
+    {
+        JobsCeiling = (buildingCounts[3] * 10) +( buildingCounts[2] * 4);
+        JobsCurrent = Mathf.Min((int)PopulationCurrent, JobsCeiling);
+    }
+
+    void CalculateCash()
+    {
+        Cash += JobsCurrent * 3;
+    }
+
+    public void DepositCash(int cash)
+    {
+        Cash += cash;
+    }
+
+    void CalculatePopulation()
+    {
+        PopulationCeiling = buildingCounts[1] * 4;
+        PopulationCurrent = Mathf.Min((int)buildingCounts[2] * 10, PopulationCeiling);
+     }
+	
+}
---
+++ Assets/Scripts/UIController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIController : MonoBehaviour {
+    [SerializeField]
+    private Text dayText;
+    [SerializeField]
+    private Text cityText;
+    private City city;
+	// Use this for initialization
+	void Start () {
+        city = GetComponent<City>();
+	}
+	public void UpdateCityData()
+    {
+       cityText.text = string.Format("Jobs: {0}/{1}\nCash: £{2} (+£{5})\nPopulation: {3}/{4}",
+            city.JobsCurrent, city.JobsCeiling, city.Cash, city.PopulationCurrent, city.PopulationCeiling, city.JobsCurrent*3);
+    }
+	public void UpdateDayCount()
+    {
+        dayText.text = string.Format("Day {0}", city.Day);
+    }
+}
